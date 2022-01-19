17fb46a15b99462aff098a93b6806ee391574a77
Anu <anuoluwa.apiti@gmail.com>
2022-01-19 18:58:26 +0100

    Update setup-config.js

--- setup-config.js
+++ setup-config.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const serviceManager = require("windows-service-manager");
 
 (function () {
-    // let result = false;
     serviceManager.queryServices(function (error, services) {
         if (error) {
             console.log("**error from querying services***", error);
@@ -16,22 +15,3 @@ const serviceManager = require("windows-service-manager");
     });
 })()
 
-// serviceManager.queryServices(function (error, services) {
-//   console.log("**error from querying services***", error);
-//   fs.writeFile('./config.js', JSON.stringify(services), function(err) {
-//     if(err) console.log(err)
-//     console.log("**error creating config file***", err)
-//   });
-//   console.log("Config file created and populated");
-// });
-
-// read from file
-
-// serviceManager.queryServices(function (error, services) {
-//   console.log("**error from querying services***", error);
-//   fs.writeFile('./config.js', JSON.stringify(services), function(err) {
-//     if(err) console.log(err)
-//     console.log("**error creating config file***", err)
-//   });
-//   console.log("Config file created and populated");
-// });
